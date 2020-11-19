2974da808dd60ac6e05126e7cf395f799d17673a
Gaddiel <alegaddiel@gmail.com>
2020-11-18 22:09:51 -0600

    version 1

---
+++ 1p/sets/nombresAleatorios.js
@@ -0,0 +1,14 @@
+let arrNombres = [
+  'Gaddiel',
+  'Armando',
+  'Nacho',
+  'Jesus',
+  'Leo',
+  'Jose',
+  'Ulises',
+  'Beatriz',
+  'Maria',
+  'Yaxkin',
+];
+let nombres = new Set(arrNombres);
+console.log(nombres);
